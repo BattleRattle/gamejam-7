cd98dd9ae8df7173deb91ddcacf513d3a3480880
matze <matze@mdoetsch.de>
2014-11-14 21:53:06 +0100

    added base structure
     - package.json
    
     - added beefy

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var http = require('http'),
+	beefy = require('beefy'),
+	config = require('./config');
+
+var server = http.createServer(
+	beefy({
+		index: './index.html',
+		entries: {
+			// list of all static js files where it should be possible to use node functionality like require()
+			'/game.js': './static/game.js'
+		},
+		cwd: __dirname + '/static/',
+		quiet: false,
+		live: true,
+		bundlerFlags: ['-t'],
+		watchify: false
+	})
+).listen(config.server.port);
+
+console.log('Server started on http://' + config.server.host + ':' + config.server.port + '/');
