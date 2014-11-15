6131c007b083ec6c351b370bb7d376da4d5a773c
Norman Soetbeer <norman.soetbeer@innogames.de>
2014-11-15 02:03:02 +0100

    we can haz fun bar juice

--- static/js/hud/FunBar.js
+++ static/js/hud/FunBar.js
@@ -8,6 +8,7 @@ function FunBar() {
     this.element = new createjs.Container();
     this.element.x = constants.GAME_WIDTH / 2 - 125;
     this.current = 0;
+	this.lastIncrease = 0;
     this.boarder = new createjs.Shape();
     this.boarder.graphics.beginFill("#444").drawRect(0, 0, 250, 50);
     this.element.addChild(this.boarder);
@@ -27,25 +28,36 @@ FunBar.prototype.onHit = function(event) {
         return;
     }
 
-    this.current++;
-    this.current = Math.min(this.current, maxValue);
+	this.increase(1);
 };
 
 FunBar.prototype.onCombo = function(event) {
-    this.current += event.level;
-    this.current = Math.min(this.current, maxValue);
+    this.increase(event.level);
+};
+
+FunBar.prototype.increase = function(value) {
+	this.current += value;
+	this.current = Math.min(this.current, maxValue);
+	this.lastIncrease = new Date().getTime();
 };
 
 FunBar.prototype.tick = function(event) {
     if (this.current > 0) {
         this.current -= (event.delta / 1000) * autoDecreasePerSecond;
         this.current = Math.max(this.current, 0);
-        this.drawFill();
+
+		var lastIncreaseDiff = event.timeStamp - this.lastIncrease;
+		if (lastIncreaseDiff < 1000) {
+			this.drawFill('rgb(' + Math.round(255 - 85 / 1000 * lastIncreaseDiff) + ', ' + Math.round(170 / 1000 * lastIncreaseDiff) + ', ' + Math.round(255 - 85 / 1000 * lastIncreaseDiff) + ')');
+		} else {
+			this.drawFill();
+		}
     }
 };
 
-FunBar.prototype.drawFill = function() {
-    this.fill.graphics.clear().beginFill("#aaa").drawRect(5, 5, (this.current / maxValue) * maxWidth, 40);
+FunBar.prototype.drawFill = function(color) {
+	color = (color === undefined) ? '#aaa' : color;
+    this.fill.graphics.clear().beginFill(color).drawRect(5, 5, (this.current / maxValue) * maxWidth, 40);
 };
 
 module.exports = FunBar;
