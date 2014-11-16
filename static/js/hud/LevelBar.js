653076ff3f97a3e68ef6d5d8ac8224273693dcb1
matze <matze@mdoetsch.de>
2014-11-16 10:19:19 +0100

    added more levels

--- static/js/hud/LevelBar.js
+++ static/js/hud/LevelBar.js
@@ -1,19 +1,14 @@
-var constants = require('../GameConsts'),
-	iconLevel = '⚔';
+var constants = require('../GameConsts');
 
 function LevelBar() {
 	this.element = new createjs.Container();
-	this.element.x = constants.GAME_WIDTH - 100;
+	this.element.x = constants.GAME_WIDTH - 130;
 	this.element.y = constants.GAME_HEIGHT - 60;
 
-	this.icon = iconLevel;
-
-	this.text = new createjs.Text(iconLevel + " ", "25px Komika", '#fff');
+	this.text = new createjs.Text(" ", "25px Komika", '#fff');
 	this.text.x = 0;
 	this.text.y = 0;
 	this.element.addChild(this.text);
-
-	this.level = 1;
 }
 
 LevelBar.prototype.registerEvents = function(emitter) {
@@ -21,8 +16,7 @@ LevelBar.prototype.registerEvents = function(emitter) {
 };
 
 LevelBar.prototype.onChangeLevel = function(level) {
-	console.log(level);
-	this.text.text = iconLevel + " " + level.levelId;
+	this.text.text = "Level " + level.levelId;
 };
 
 module.exports = LevelBar;
